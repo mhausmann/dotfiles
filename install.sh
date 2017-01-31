12c40591c680eb09603f2c37870c4a6b3eb702c7
Matt Hausmann <github@u6.co.za>
2017-01-31 12:07:27 +0000

    Version 0.1

---
+++ install.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+
+# include my library helpers for colorized echo and require_brew, etc
+source ./lib_sh/echos.sh
+source ./lib_sh/requirers.sh
+
+bot "Hi! I'm going to install tooling and tweak your system settings. Here I go..."
+
+# ensure ~/.gitshots exists to prevent "Error." in terminal after git commits
+# TODO: use node/yoeman to templatize elements of this project and use inquerer to ask if the
+# user wants to use gitshots
+mkdir -p ~/.gitshots
+
+# Ask for the administrator password upfront
+if sudo grep -q "# %wheel\tALL=(ALL) NOPASSWD: ALL" "/etc/sudoers"; then
+
+  # Ask for the administrator password upfront
+  bot "I need you to enter your sudo password so I can install some things:"
+  sudo -v
+
+  # Keep-alive: update existing sudo time stamp until the script has finished
+  while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Install command-line tools using Homebrew.
+xcode-select --install
+sleep 1
+osascript <<EOD
+  tell application "System Events"
+    tell process "Install Command Line Developer Tools"
+      keystroke return
+      click button "Agree" of window "License Agreement"
+    end tell
+  end tell
+EOD
+
+# Install Homebrew
+if [[ `uname` == 'Darwin' ]]; then
+  which -s brew
+  if [[ $? != 0 ]]; then
+    echo 'Installing Homebrew...'
+      ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+      brew update
+  fi
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed --with-default-names
+# Install Bash 4.
+# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
+# running `chsh`.
+brew install bash
+brew tap homebrew/versions
+brew install bash-completion2
+
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
+  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
+  chsh -s /usr/local/bin/bash;
+fi;
+
+# Install `wget` with IRI support.
+brew install wget --with-iri
+
+# Install more recent versions of some macOS tools.
+brew install vim --with-override-system-vi
+brew install homebrew/dupes/grep
+brew install homebrew/dupes/openssh
+brew install homebrew/dupes/screen
+
+# Install other useful binaries.
+brew install ssh-copy-id
+brew install tree
+
+# Install Cask
+brew tap caskroom/cask
+
+# Install a few Cask apps
+brew cask install google-ehrome
+brew cask install sublime-text
+
+# Remove outdated versions from the cellar.
+brew cleanup
