2e6ca2e2467d7425120d7cce208765e11a891651
Matt Hausmann <github@u6.co.za>
2017-01-31 12:50:23 +0000

    Aesthetic updates to scripts

--- install.sh
+++ install.sh
@@ -6,11 +6,6 @@ source ./lib_sh/requirers.sh
 
 bot "Hi! I'm going to install tooling and tweak your system settings. Here I go..."
 
-# ensure ~/.gitshots exists to prevent "Error." in terminal after git commits
-# TODO: use node/yoeman to templatize elements of this project and use inquerer to ask if the
-# user wants to use gitshots
-mkdir -p ~/.gitshots
-
 # Ask for the administrator password upfront
 if sudo grep -q "# %wheel\tALL=(ALL) NOPASSWD: ALL" "/etc/sudoers"; then
 
@@ -21,7 +16,7 @@ if sudo grep -q "# %wheel\tALL=(ALL) NOPASSWD: ALL" "/etc/sudoers"; then
   # Keep-alive: update existing sudo time stamp until the script has finished
   while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Install command-line tools using Homebrew.
+running "Installing XCode command-line tools."
 xcode-select --install
 sleep 1
 osascript <<EOD
@@ -33,57 +28,93 @@ osascript <<EOD
   end tell
 EOD
 
-# Install Homebrew
-if [[ `uname` == 'Darwin' ]]; then
-  which -s brew
-  if [[ $? != 0 ]]; then
-    echo 'Installing Homebrew...'
-      ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-      brew update
+running "checking homebrew install"
+brew_bin=$(which brew) 2>&1 > /dev/null
+if [[ $? != 0 ]]; then
+  action "installing homebrew"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    if [[ $? != 0 ]]; then
+      error "unable to install homebrew, script $0 abort!"
+      exit 2
   fi
+else
+  ok
+  # Make sure we’re using the latest Homebrew
+  running "updating homebrew"
+  brew update
+  ok
+  bot "before installing brew packages, we can upgrade any outdated packages."
+  read -r -p "run brew upgrade? [y|N] " response
+  if [[ $response =~ ^(y|yes|Y) ]];then
+      # Upgrade any already-installed formulae
+      action "upgrade brew packages..."
+      brew upgrade
+      ok "brews updated..."
+  else
+      ok "skipped brew package upgrades.";
+  fi
+fi
 
-# Install GNU core utilities (those that come with macOS are outdated).
+#####
+# install brew cask (UI Packages)
+#####
+running "checking brew-cask install"
+output=$(brew tap | grep cask)
+if [[ $? != 0 ]]; then
+  action "installing brew-cask"
+  require_brew caskroom/cask/brew-cask
+fi
+brew tap caskroom/versions > /dev/null 2>&1
+ok
+
+# skip those GUI clients, git command-line all the way
+require_brew git
+# need fontconfig to install/build fonts
+require_brew fontconfig
+
+running "Installing GNU core utilities (those that come with macOS are outdated)."
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
 brew install coreutils
 
-# Install some other useful utilities like `sponge`.
+running "Installing some other useful utilities like `sponge`."
 brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+runnign "Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed."
 brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
+running "Install GNU `sed`, overwriting the built-in `sed`."
 brew install gnu-sed --with-default-names
-# Install Bash 4.
+running "Install Bash 4."
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
 # running `chsh`.
 brew install bash
 brew tap homebrew/versions
 brew install bash-completion2
 
-# Switch to using brew-installed bash as default shell
+running "Switch to using brew-installed bash as default shell"
 if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
   echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
   chsh -s /usr/local/bin/bash;
 fi;
 
-# Install `wget` with IRI support.
+running "Installing `wget` with IRI support."
 brew install wget --with-iri
 
-# Install more recent versions of some macOS tools.
+running "Installing more recent versions of some macOS tools."
 brew install vim --with-override-system-vi
 brew install homebrew/dupes/grep
 brew install homebrew/dupes/openssh
 brew install homebrew/dupes/screen
 
-# Install other useful binaries.
+running "Installing other useful binaries."
 brew install ssh-copy-id
 brew install tree
 
-# Install Cask
-brew tap caskroom/cask
-
-# Install a few Cask apps
+running "Installing a few Cask apps."
 brew cask install google-ehrome
 brew cask install sublime-text
+brew cask install 1password
 
-# Remove outdated versions from the cellar.
+running "Remove outdated versions from the Brew cellar."
 brew cleanup
+
+running "About to set system preferences from .macos file."
+source .macos
